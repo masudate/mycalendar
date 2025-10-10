7b14f2f9f07fe2a8d2070fb8d13daa76bc224d38
“masudate” <m.masudate@gmail.com>
2025-10-10 10:21:44 +0900

    ポートフォリオ用ページのURL変更

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,17 +10,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('diary/', include('diary.urls')), 
-    path('', diary_views.calendar_view, name='home'),
+    path('home', diary_views.calendar_view, name='home'),
+    path('calendar/', diary_views.calendar_view, name='calendar'),
     path('login/', diary_views.login_view, name='login'),
     path("logout/", diary_views.logout_view, name="logout"),
     path("signup/", diary_views.signup_view, name="signup"),
-    path('portfolio/', diary_views.portfolio, name='portfolio'),
+    path('', diary_views.portfolio, name='portfolio'),
 ]
 
 
 if settings.DEBUG:
-    # /static 用：staticfiles のファインダー経由（STATICFILES_DIRS を含めて解決）
     urlpatterns += staticfiles_urlpatterns()
 
-    # /media 用：MEDIA_ROOT を配信
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
